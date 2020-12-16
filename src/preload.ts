85a8e98f9c02fc9988f141e29c10a665676e7f2e
Marco Ippolito <ippolito.marco@gmail.com>
2020-12-16 18:21:50 +0100

    commit

--- src/preload.ts
+++ src/preload.ts
@@ -8,7 +8,7 @@ contextBridge.exposeInMainWorld(
           ipcRenderer.invoke(channel, data).catch(e => console.log(e))
       },
       receive: (channel, func) => {
-        ipcRenderer.invoke(channel, (event, ...args) => func(...args)).catch(e => console.log(e))
+        ipcRenderer.on(channel, (event, ...args) => func(...args)).catch(e => console.log(e))
       }
   }
 )
